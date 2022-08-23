b05bb03181140553d7bbdfc05e69f5833db312f8
Rohan Kashyap <41742578+the-rk9@users.noreply.github.com>
2022-08-23 12:38:51 +0530

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-        apiKey: "AIzaSyAeQioxA7l84L7W0v4nMYoZCicsqVyT-d0",
-        authDomain: "dummy-project-c20d8.firebaseapp.com",
-        databaseURL: "https://dummy-project-c20d8-default-rtdb.firebaseio.com",
-        projectId: "dummy-project-c20d8",
-        storageBucket: "dummy-project-c20d8.appspot.com",
-        messagingSenderId: "887819222804",
-        appId: "1:887819222804:web:1fb498348581d2e9020c01",
-        measurementId: "G-VGLHS594WZ"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
